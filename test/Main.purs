7d1307eecf27c8842106c7a04f22fc6ce22319e3
hoangpq <pquochoang2007@gmail.com>
2022-01-30 11:39:45 +0700

    Add some test case

--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,15 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+-- import Effect.Class.Console (log)
+import Test.Unit (suite, test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
 
 main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+  runTest do
+    suite "equal" do
+      test "2 + 3 = 5" do
+        Assert.equal 5 (2 + 3)
+        
